b79ba0b1f0d532e8a449e55f88026072e1bd7679
Imran Iqbal <imran1337@gmail.com>
2019-08-06 21:57:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function sayHiToGrandma(string) {
   return"I can't hear you!"
 } else if (`${string}`=== uppercase) {
   return"YES INDEED!"
-} else if (`${string}` === I love you, Grandma.) {
+} else if (`${string}` === `I love you, Grandma.`) {
   return"I love you,too"
 }}
 
